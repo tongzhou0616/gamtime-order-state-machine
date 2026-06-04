f4678aa829a4bb17cf218cd138f69b2bbe11e71c
Tong Zhou <tongzhou930616@gmail.com>
2026-06-04 11:35:00 -0400

    feat(payment): add injectable stub for tests

--- internal/payment/payment.go
+++ internal/payment/payment.go
@@ -2,10 +2,41 @@ package payment
 
 import "errors"
 
-var ErrPaymentDeclined = errors.New("payment declined")
+var (
+	ErrPaymentDeclined  = errors.New("payment declined")
+	ErrCompletionFailed = errors.New("completion failed")
+	ErrVoidFailed       = errors.New("void failed")
+)
 
 type Payment interface {
 	Authorize(orderID string) error
 	Complete(orderID string) error
 	Void(orderID string) error
+}
+
+type Stub struct {
+	AuthorizeFn func(string) error
+	CompleteFn  func(string) error
+	VoidFn      func(string) error
+}
+
+func (s *Stub) Authorize(orderID string) error {
+	if s.AuthorizeFn != nil {
+		return s.AuthorizeFn(orderID)
+	}
+	return nil
+}
+
+func (s *Stub) Complete(orderID string) error {
+	if s.CompleteFn != nil {
+		return s.CompleteFn(orderID)
+	}
+	return nil
+}
+
+func (s *Stub) Void(orderID string) error {
+	if s.VoidFn != nil {
+		return s.VoidFn(orderID)
+	}
+	return nil
 }
