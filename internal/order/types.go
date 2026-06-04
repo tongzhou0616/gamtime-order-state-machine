ea9804b22004950d7f56d295cda325f5da457537
Tong Zhou <tongzhou930616@gmail.com>
2026-06-04 11:00:00 -0400

    feat(order): add Order model and terminal check

--- internal/order/types.go
+++ internal/order/types.go
@@ -13,9 +13,24 @@ const (
 	StateNeedsAttention     State = "needs_attention"
 )
 
+func (s State) IsTerminal() bool {
+	switch s {
+	case StateComplete, StateRejected, StateCancelled, StateNeedsAttention:
+		return true
+	default:
+		return false
+	}
+}
+
 type HistoryEntry struct {
 	From State     `json:"from"`
 	To   State     `json:"to"`
 	At   time.Time `json:"at"`
 	Note string    `json:"note,omitempty"`
+}
+
+type Order struct {
+	ID      string         `json:"id"`
+	State   State          `json:"state"`
+	History []HistoryEntry `json:"history"`
 }
