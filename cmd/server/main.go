954459627ebe7b474f1f90d916d22b710b6ceaae
Tong Zhou <tongzhou930616@gmail.com>
2026-06-04 13:32:00 -0400

    feat(server): support PORT and env-driven payment failures

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/gametime/order-state-machine/internal/api"
 	"github.com/gametime/order-state-machine/internal/order"
@@ -10,13 +12,31 @@ import (
 )
 
 func main() {
+	stub := payment.NewStubFromEnv(
+		envBool("PAYMENT_AUTHORIZE_FAIL"),
+		envBool("COMPLETE_FAIL"),
+		envBool("VOID_FAIL"),
+	)
+
 	store := order.NewMemoryStore()
-	svc := order.NewService(store, &payment.Stub{})
+	svc := order.NewService(store, stub)
 	handler := api.NewHandler(svc)
 
 	mux := http.NewServeMux()
 	handler.Register(mux)
 
-	log.Println("listening on :8080")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	addr := ":8080"
+	if v := os.Getenv("PORT"); v != "" {
+		addr = ":" + v
+	}
+
+	log.Printf("listening on %s", addr)
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func envBool(key string) bool {
+	v, _ := strconv.ParseBool(os.Getenv(key))
+	return v
 }
